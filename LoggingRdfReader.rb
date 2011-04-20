a5c87aaa08db9a37d9d9de4a77e3e20ed586f1d7
cmader <christian.mader@univie.ac.at>
2011-04-20 12:33:22 +0200

    performance improvements

---
+++ LoggingRdfReader.rb
@@ -0,0 +1,44 @@
+class LoggingRdfReader
+
+	def initialize(rdfReader, log)
+		@rdfReader = rdfReader
+		@log = log
+	end
+
+	def loopStatements
+		i = 1;
+		@prevPercentage = 0
+
+		@rdfReader.each_statement do |statement|
+			if (@totalStatements == nil)
+				outputProcessedTriples(i)
+			else
+				outputPercentage(i)
+			end
+			i += 1
+
+			yield(statement)
+		end
+
+		@totalStatements = i
+	end
+
+	private
+
+	def outputProcessedTriples(count)
+		if (count % 5000 == 0) 
+			@log.info("processed >#{count} triples")
+		end
+	end
+
+	def outputPercentage(count)
+		percentage = Integer((count.fdiv(@totalStatements)) * 100)
+
+		if (percentage % 10 == 0 && percentage > @prevPercentage)
+			@log.info("#{percentage}% finished")
+		
+			@prevPercentage = percentage
+		end
+	end
+
+end
---
+++ LooseConceptFinder.rb
@@ -0,0 +1,38 @@
+require_relative 'LoggingRdfReader'
+
+class LooseConceptFinder
+
+	def initialize(loggingRdfReader, log, allConcepts)
+		log.info("identifying loose concepts")
+
+		@reader = loggingRdfReader
+		@log = log
+		@looseConcepts = Array.new(allConcepts)
+
+		findLooseConcepts
+	end
+	
+	def getLooseConcepts
+		@looseConcepts
+	end
+
+	private
+
+	def findLooseConcepts
+		@reader.loopStatements do |statement|
+			if (isSkosPredicate(statement.predicate))
+				if (@looseConcepts.include?(statement.subject))
+					@looseConcepts.delete(statement.subject)
+				end
+				if (@looseConcepts.include?(statement.object))
+					@looseConcepts.delete(statement.object)
+				end
+			end
+		end
+	end
+
+	def isSkosPredicate(predicate)
+		return predicate.to_s.include?("skos")
+	end
+
+end
