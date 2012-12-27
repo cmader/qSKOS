fc97451c04b054f5d2803929281df84999755844
cmader <christian.mader@univie.ac.at>
2012-12-27 14:43:45 +0100

    implemented class cast exception catching

--- src/main/java/at/ac/univie/mminf/qskos4j/issues/InverseRelationsChecker.java
+++ src/main/java/at/ac/univie/mminf/qskos4j/issues/InverseRelationsChecker.java
@@ -6,9 +6,12 @@ import at.ac.univie.mminf.qskos4j.util.vocab.SparqlPrefix;
 import at.ac.univie.mminf.qskos4j.util.vocab.VocabRepository;
 import org.openrdf.OpenRDFException;
 import org.openrdf.model.Resource;
+import org.openrdf.model.Value;
 import org.openrdf.query.BindingSet;
 import org.openrdf.query.QueryEvaluationException;
 import org.openrdf.query.TupleQueryResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,7 +28,8 @@ public class InverseRelationsChecker extends Issue {
         {"skos:exactMatch", "skos:exactMatch"},
         {"skos:closeMatch", "skos:closeMatch"}
     };
-	
+
+    private final Logger logger = LoggerFactory.getLogger(InverseRelationsChecker.class);
 	private Map<Pair<Resource>, String> omittedInverseRelations = new HashMap<Pair<Resource>, String>();
 
 	public InverseRelationsChecker(VocabRepository vocabRepository) {
@@ -37,7 +41,7 @@ public class InverseRelationsChecker extends Issue {
 	{
 		for (String[] inversePropertyPair : inversePropertyPairs) {
 			TupleQueryResult result = vocabRepository.query(createOmittedRelationsQuery(inversePropertyPair));
-			addToOmittedReverseRelationsMap(result, inversePropertyPair);			
+			addToOmittedInverseRelationsMap(result, inversePropertyPair);
 		}
 		
 		return new UnidirRelResourcesResult(omittedInverseRelations);
@@ -57,19 +61,34 @@ public class InverseRelationsChecker extends Issue {
 				"}";	
 	}
 	
-	private void addToOmittedReverseRelationsMap(
+	private void addToOmittedInverseRelationsMap(
 		TupleQueryResult result, 
 		String[] inversePropertyPair) throws QueryEvaluationException 
 	{
 		while (result.hasNext()) {
 			BindingSet queryResult = result.next();
-			Resource resource1 = (Resource) queryResult.getValue("resource1");
-			Resource resource2 = (Resource) queryResult.getValue("resource2");
-			
-			omittedInverseRelations.put(
-				new Pair<Resource>(resource1, resource2), 
-				inversePropertyPair[0] +"/"+ inversePropertyPair[1]);
-		}
+
+            Value value1 = queryResult.getValue("resource1");
+            Value value2 = queryResult.getValue("resource2");
+            String inverseProperties = inversePropertyPair[0] +"/"+ inversePropertyPair[1];
+
+            addToMap(value1, value2, inverseProperties);
+        }
+    }
+
+    private void addToMap(Value value1, Value value2, String inverseProperties)
+    {
+        try {
+            Resource resource1 = (Resource) value1;
+            Resource resource2 = (Resource) value2;
+
+            omittedInverseRelations.put(
+                new Pair<Resource>(resource1, resource2),
+                    inverseProperties);
+        }
+        catch (ClassCastException e) {
+            logger.info("resource expected for relation " +inverseProperties+ " (" +value1+ " <-> " +value2+ ")");
+        }
 	}
 
 }
