111e27722b4e2f285242530ccc6b8d1766e469fe
cmader <christian.mader@univie.ac.at>
2013-01-29 12:24:13 +0100

    changed issue abstract superclass to establish an infrastructure for pluggable issues, i.e. issues that can be added and removed dynamically

--- src/main/java/at/ac/univie/mminf/qskos4j/issues/conceptscheme/TopConceptsHavingBroaderConcepts.java
+++ src/main/java/at/ac/univie/mminf/qskos4j/issues/conceptscheme/TopConceptsHavingBroaderConcepts.java
@@ -1,4 +1,8 @@
 package at.ac.univie.mminf.qskos4j.issues.conceptscheme;
 
+/**
+ * Finds top concepts that have broader concepts (
+ * <a href="https://github.com/cmader/qSKOS/wiki/Quality-Issues#wiki-Top_Concepts_Having_Broader_Concepts">Top Concepts Having Broader Concepts</a>
+ */
 public class TopConceptsHavingBroaderConcepts {
 }
