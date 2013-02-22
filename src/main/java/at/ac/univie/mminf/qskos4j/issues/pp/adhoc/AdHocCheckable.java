b020fed6d8ec0790b6e16d37f877577cf8ff6efd
cmader <christian.mader@univie.ac.at>
2013-02-22 16:26:41 +0100

    changed visibility

--- src/main/java/at/ac/univie/mminf/qskos4j/issues/pp/adhoc/AdHocCheckable.java
+++ src/main/java/at/ac/univie/mminf/qskos4j/issues/pp/adhoc/AdHocCheckable.java
@@ -4,7 +4,7 @@ import at.ac.univie.mminf.qskos4j.issues.IssueOccursException;
 import org.openrdf.OpenRDFException;
 import org.openrdf.model.Statement;
 
-interface AdHocCheckable {
+public interface AdHocCheckable {
 
     public abstract void checkStatement(Statement statement) throws IssueOccursException, OpenRDFException;
 
