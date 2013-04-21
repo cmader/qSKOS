5272983feb8e29fabd78d3f26f804a14b0247d03
cmader <mail@christianmader.net>
2013-04-21 18:29:19 +0200

    added test for documentation subproperty

--- src/test/java/at/ac/univie/mminf/qskos4j/issues/UndocumentedConceptsTest.java
+++ src/test/java/at/ac/univie/mminf/qskos4j/issues/UndocumentedConceptsTest.java
@@ -8,6 +8,7 @@ import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openrdf.OpenRDFException;
+import org.openrdf.repository.RepositoryConnection;
 
 import java.io.IOException;
 
@@ -15,15 +16,19 @@ import java.io.IOException;
 public class UndocumentedConceptsTest {
 
 	private UndocumentedConcepts undocumentedConcepts;
+    private RepositoryConnection repCon;
 	
 	@Before
 	public void setUp() throws OpenRDFException, IOException {
+        repCon = new RepositoryBuilder().setUpFromTestResource("documentedConcepts.rdf").getConnection();
         undocumentedConcepts = new UndocumentedConcepts(new AuthoritativeConcepts(new InvolvedConcepts()));
-        undocumentedConcepts.setRepositoryConnection(new RepositoryBuilder().setUpFromTestResource("documentedConcepts.rdf").getConnection());
+        undocumentedConcepts.setRepositoryConnection(repCon);
 	}
 	
 	@Test
 	public void testAverageDocumentationCoverageRatio() throws OpenRDFException {
+        System.out.println(repCon.getStatements(null,null,null,true).asList());
+
 		Assert.assertEquals(1, undocumentedConcepts.getPreparedData().size());
 	}
 	
