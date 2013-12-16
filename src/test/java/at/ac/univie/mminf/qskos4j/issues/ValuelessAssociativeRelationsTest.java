0343c5e83f990df317fbecb9d2c9db0ec660152f
cmader <mail@christianmader.net>
2013-12-16 22:46:57 +0100

    Merge branch 'devel'
    Conflicts:
    	pom.xml
    	src/main/java/at/ac/univie/mminf/qskos4j/issues/relations/UnidirectionallyRelatedConcepts.java
    	src/main/java/at/ac/univie/mminf/qskos4j/issues/relations/ValuelessAssociativeRelations.java

--- src/test/java/at/ac/univie/mminf/qskos4j/issues/ValuelessAssociativeRelationsTest.java
+++ src/test/java/at/ac/univie/mminf/qskos4j/issues/ValuelessAssociativeRelationsTest.java
@@ -7,7 +7,7 @@ import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openrdf.OpenRDFException;
-import org.openrdf.model.Resource;
+import org.openrdf.model.URI;
 
 import java.io.IOException;
 import java.util.Collection;
@@ -24,7 +24,7 @@ public class ValuelessAssociativeRelationsTest {
 
     @Test
     public void testRedundantAssociativeRelationsCount() throws OpenRDFException {
-        Collection<Pair<Resource>> redAssRels = valuelessAssociativeRelations.getResult();
-        Assert.assertEquals(7, redAssRels.size());
+        Collection<Pair<URI>> redAssRels = valuelessAssociativeRelations.getResult();
+        Assert.assertEquals(6, redAssRels.size());
     }
 }
