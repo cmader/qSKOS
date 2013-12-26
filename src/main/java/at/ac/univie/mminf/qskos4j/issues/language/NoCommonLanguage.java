bad933d35835b2d004aa31da42736a12002b29ae
cmader <mail@christianmader.net>
2013-12-26 11:40:02 +0100

    started implementation of common language test

--- src/main/java/at/ac/univie/mminf/qskos4j/issues/language/NoCommonLanguage.java
+++ src/main/java/at/ac/univie/mminf/qskos4j/issues/language/NoCommonLanguage.java
@@ -1,5 +1,21 @@
 package at.ac.univie.mminf.qskos4j.issues.language;
 
-public class NoCommonLanguage {
-    //TODO: implement me!
+import at.ac.univie.mminf.qskos4j.issues.Issue;
+import at.ac.univie.mminf.qskos4j.result.Result;
+import org.openrdf.OpenRDFException;
+
+public class NoCommonLanguage extends Issue {
+
+    public NoCommonLanguage() {
+        super("ncl",
+              "No Common Language",
+              "Checks if concept labels are available in at least one common language",
+              IssueType.ANALYTICAL);
+    }
+
+    @Override
+    protected Result<?> invoke() throws OpenRDFException {
+        return null;
+    }
+
 }
