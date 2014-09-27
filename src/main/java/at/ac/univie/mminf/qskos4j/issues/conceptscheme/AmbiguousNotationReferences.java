3a4a99ed9c7470bd6883e21347262b0c546614d6
cmader <mail@christianmader.net>
2014-09-27 11:11:59 +0200

    refactorings, removed disabled broken links check

--- src/main/java/at/ac/univie/mminf/qskos4j/issues/conceptscheme/AmbiguousNotationReferences.java
+++ src/main/java/at/ac/univie/mminf/qskos4j/issues/conceptscheme/AmbiguousNotationReferences.java
@@ -12,6 +12,7 @@ import org.openrdf.model.Resource;
 import org.openrdf.model.Statement;
 import org.openrdf.model.impl.URIImpl;
 import org.openrdf.model.vocabulary.SKOS;
+import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.RepositoryResult;
 
 import java.util.ArrayList;
@@ -38,37 +39,72 @@ public class AmbiguousNotationReferences extends Issue<CollectionResult<Ambiguou
 
     @Override
     protected CollectionResult<AmbiguousNotation> invoke() throws OpenRDFException {
-        Set<AmbiguousNotation> problematicRelations = new HashSet<>();
+        Set<AmbiguousNotation> ambiguousNotations = new HashSet<>();
 
         for (Resource authConcept : authoritativeConcepts.getResult().getData()) {
-            RepositoryResult<Statement> conceptsWithNotation = repCon.getStatements(authConcept, SKOS.NOTATION, null, false);
-
-            Collection<Literal> notationsForConcept = new ArrayList<>();
-            while (conceptsWithNotation.hasNext()) {
-                Literal notationLiteral = (Literal) conceptsWithNotation.next().getObject();
-                notationsForConcept.add(notationLiteral);
-
-                RepositoryResult<Statement> conceptsWithSameNotation = repCon.getStatements(null, SKOS.NOTATION, notationLiteral, false);
-                while (conceptsWithSameNotation.hasNext()) {
-                    Resource conflictingResource = conceptsWithSameNotation.next().getSubject();
-
-                    if (conflictingResource.equals(authConcept)) continue;
-
-                    if (ConceptSchemeUtil.inSameConceptScheme(authConcept, conflictingResource, repCon) ||
-                        ConceptSchemeUtil.inNoConceptScheme(authConcept, conflictingResource, repCon))
-                    {
-                        problematicRelations.add(new AmbiguousNotationMultipleResources(authConcept, conflictingResource, notationLiteral));
-                    }
-                }
-            }
-
-            if (notationsForConcept.size() > 1) {
-                problematicRelations.add(new AmbiguousNotationWithinOneResource(authConcept, notationsForConcept));
-            }
-
+            ambiguousNotations.addAll(checkNotationsForConcept(authConcept));
         }
 
-        return new CollectionResult<>(problematicRelations);
+        return new CollectionResult<>(ambiguousNotations);
+    }
+
+    private Set<AmbiguousNotation> checkNotationsForConcept(Resource concept) throws OpenRDFException {
+        Set<AmbiguousNotation> ambiguousNotations = new HashSet<>();
+
+        ambiguousNotations.addAll(findAmbiguousNotationsWithinResource(concept));
+        ambiguousNotations.addAll(findAmbiguousNotationsInMultipleResources(concept));
+
+        return ambiguousNotations;
+    }
+
+    private Set<AmbiguousNotation> findAmbiguousNotationsWithinResource(Resource concept) throws RepositoryException {
+        Set<AmbiguousNotation> ambiguousNotations = new HashSet<>();
+        RepositoryResult<Statement> notations = repCon.getStatements(concept, SKOS.NOTATION, null, false);
+
+        Collection<Literal> notationsForConcept = new ArrayList<>();
+        while (notations.hasNext()) {
+            Literal notationLiteral = (Literal) notations.next().getObject();
+            notationsForConcept.add(notationLiteral);
+        }
+
+        if (notationsForConcept.size() > 1) {
+            ambiguousNotations.add(new AmbiguousNotationWithinOneResource(concept, notationsForConcept));
+        }
+
+        return ambiguousNotations;
+    }
+
+    private Set<AmbiguousNotation> findAmbiguousNotationsInMultipleResources(Resource concept) throws OpenRDFException {
+        Set<AmbiguousNotation> ambiguousNotations = new HashSet<>();
+
+        RepositoryResult<Statement> notationsForConcept = repCon.getStatements(concept, SKOS.NOTATION, null, false);
+        while (notationsForConcept.hasNext()) {
+            Literal notationLiteral = (Literal) notationsForConcept.next().getObject();
+            ambiguousNotations.addAll(getNotationClashes(concept, notationLiteral));
+        }
+
+        return ambiguousNotations;
+    }
+
+    private Set<AmbiguousNotation> getNotationClashes(Resource concept, Literal notationLiteral)
+        throws OpenRDFException
+    {
+        Set<AmbiguousNotation> ambiguousNotations = new HashSet<>();
+
+        RepositoryResult<Statement> conceptsWithSameNotation = repCon.getStatements(null, SKOS.NOTATION, notationLiteral, false);
+        while (conceptsWithSameNotation.hasNext()) {
+            Resource conflictingResource = conceptsWithSameNotation.next().getSubject();
+
+            if (conflictingResource.equals(concept)) continue;
+
+            if (ConceptSchemeUtil.inSameConceptScheme(concept, conflictingResource, repCon) ||
+                ConceptSchemeUtil.inNoConceptScheme(concept, conflictingResource, repCon))
+            {
+                ambiguousNotations.add(new AmbiguousNotationMultipleResources(concept, conflictingResource, notationLiteral));
+            }
+        }
+
+        return ambiguousNotations;
     }
 
 }
