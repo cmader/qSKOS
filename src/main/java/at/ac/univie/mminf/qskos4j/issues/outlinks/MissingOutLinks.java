1bfcc29022b6fa395a04491dee830b34b60e286a
Christian Mader <mail@christianmader.net>
2017-06-23 20:37:38 +0200

    Merge branch 'rdf4j-migration' of https://github.com/tfrancart/qSKOS into tfrancart-rdf4j-migration
    # Conflicts:
    #	pom.xml
    #	src/main/java/at/ac/univie/mminf/qskos4j/cmd/ReportCollector.java
    #	src/main/java/at/ac/univie/mminf/qskos4j/cmd/VocEvaluate.java
    #	src/main/java/at/ac/univie/mminf/qskos4j/issues/conceptscheme/MappingRelationsMisuse.java
    #	src/main/java/at/ac/univie/mminf/qskos4j/issues/conceptscheme/OmittedTopConcepts.java
    #	src/main/java/at/ac/univie/mminf/qskos4j/issues/labels/EmptyLabeledResources.java
    #	src/main/java/at/ac/univie/mminf/qskos4j/issues/outlinks/MissingOutLinks.java
    #	src/main/java/at/ac/univie/mminf/qskos4j/issues/relations/ReflexivelyRelatedConcepts.java
    #	src/main/java/at/ac/univie/mminf/qskos4j/issues/relations/SolelyTransitivelyRelatedConcepts.java
    #	src/main/java/at/ac/univie/mminf/qskos4j/issues/relations/ValuelessAssociativeRelations.java
    #	src/main/java/at/ac/univie/mminf/qskos4j/issues/skosintegrity/HierarchicalRedundancy.java
    #	src/test/java/at/ac/univie/mminf/qskos4j/issues/BrokenLinksTest.java
    #	src/test/java/at/ac/univie/mminf/qskos4j/issues/DisconnectedConceptClustersTest.java
    #	src/test/java/at/ac/univie/mminf/qskos4j/issues/HierarchicalRedundancyTest.java

--- src/main/java/at/ac/univie/mminf/qskos4j/issues/outlinks/MissingOutLinks.java
+++ src/main/java/at/ac/univie/mminf/qskos4j/issues/outlinks/MissingOutLinks.java
@@ -9,7 +9,7 @@ import org.eclipse.rdf4j.RDF4JException;
 import org.eclipse.rdf4j.model.Resource;
 import org.eclipse.rdf4j.model.Statement;
 import org.eclipse.rdf4j.model.URI;
-import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.Value;p
 import org.eclipse.rdf4j.model.impl.URIImpl;
 import org.eclipse.rdf4j.repository.RepositoryException;
 import org.eclipse.rdf4j.repository.RepositoryResult;
