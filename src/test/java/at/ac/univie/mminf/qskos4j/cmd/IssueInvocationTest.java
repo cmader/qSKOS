eb837dbf123544df7fc12d1a345913107c21d1ea
larsgsvensson <lars.g.svensson@gmail.com>
2017-10-12 17:26:10 +0200

    Updated test not to use hard-coded paths that work only on *nix systems

--- src/test/java/at/ac/univie/mminf/qskos4j/cmd/IssueInvocationTest.java
+++ src/test/java/at/ac/univie/mminf/qskos4j/cmd/IssueInvocationTest.java
@@ -1,49 +1,56 @@
 package at.ac.univie.mminf.qskos4j.cmd;
 
-import at.ac.univie.mminf.qskos4j.issues.Issue;
-import at.ac.univie.mminf.qskos4j.util.IssueDescriptor;
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Files;
+
 import junit.framework.Assert;
+
 import org.junit.Test;
 
-import java.net.URL;
+import at.ac.univie.mminf.qskos4j.util.IssueDescriptor;
 
 public class IssueInvocationTest {
 
-    @Test
-    public void testAllIssuesInvocation() {
-        for (IssueDescriptor.IssueType issueType : IssueDescriptor.IssueType.values()) {
-            invokeOnCmdLine(issueType);
-        }
-    }
+	@Test
+	public void testAllIssuesInvocation() {
+		for (final IssueDescriptor.IssueType issueType : IssueDescriptor.IssueType
+				.values()) {
+			invokeOnCmdLine(issueType);
+		}
+	}
 
-    private void invokeOnCmdLine(IssueDescriptor.IssueType issueType) {
-        String command = findCommand(issueType);
-        String testFileName = getTestFileName();
+	private void invokeOnCmdLine(final IssueDescriptor.IssueType issueType) {
+		final String command = findCommand(issueType);
+		final String testFileName = getTestFileName();
+		try {
+			final File outputDirectory = Files.createTempFile("temp", "bla")
+					.toFile();
+			outputDirectory.setExecutable(true, true);
+			outputDirectory.setWritable(true, true);
+			outputDirectory.setReadable(true, true);
+			new VocEvaluate(new String[] { command, "-o",
+					outputDirectory.getCanonicalPath(), testFileName });
+		} catch (final Exception e) {
+			Assert.fail(e.getMessage() + ", command: " + command);
+		}
+	}
 
-        try {
-            new VocEvaluate(new String[] {command, "-o", "/tmp/testreport", testFileName});
-        }
-        catch (Exception e) {
-            Assert.fail(e.getMessage() + ", command: " +command);
-        }
-    }
+	private String findCommand(final IssueDescriptor.IssueType type) {
+		switch (type) {
+		case ANALYTICAL:
+			return VocEvaluate.CMD_NAME_ANALYZE;
 
-    private String findCommand(IssueDescriptor.IssueType type) {
-        switch (type)
-        {
-            case ANALYTICAL:
-                return VocEvaluate.CMD_NAME_ANALYZE;
+		case STATISTICAL:
+			return VocEvaluate.CMD_NAME_SUMMARIZE;
+		}
 
-            case STATISTICAL:
-                return VocEvaluate.CMD_NAME_SUMMARIZE;
-        }
+		throw new IllegalStateException("unknown measure type");
+	}
 
-        throw new IllegalStateException("unknown measure type");
-    }
-
-    private String getTestFileName() {
-        URL conceptsUrl = getClass().getResource("/nocontent.rdf");
-        return conceptsUrl.getFile();
-    }
+	private String getTestFileName() {
+		final URL conceptsUrl = getClass().getResource("/nocontent.rdf");
+		return conceptsUrl.getFile();
+	}
 
 }
